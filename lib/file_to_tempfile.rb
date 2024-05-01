b697c7e2a4a332175b8a7038d2590e5331a95ee0
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-05-01 17:32:12 -0300

    Apply linting

--- lib/file_to_tempfile.rb
+++ lib/file_to_tempfile.rb
@@ -2,17 +2,18 @@
 
 require_relative "file_to_tempfile/version"
 
-require 'tempfile'
+require "tempfile"
 
+# This module provides a method to convert a File object to a Tempfile object.
 module FileToTempfile
-  def self.convert(file, using_path = false)
-    raise ArgumentError, 'Expected a File object as the first argument' unless file.is_a? File
-    raise ArgumentError, 'Expected a boolean as the second argument' unless [true, false].include?(using_path)
-    raise IOError, 'The file must be open before it can be converted' if file.closed?
+  def self.convert(file, using_path: false)
+    raise ArgumentError, "Expected a File object as the first argument" unless file.is_a? File
+    raise ArgumentError, "Expected a boolean as the second argument" unless [true, false].include?(using_path)
+    raise IOError, "The file must be open before it can be converted" if file.closed?
 
     file.rewind if file.eof?
 
-    tempfile = Tempfile.new(using_path ? file.path : '').tap do |t|
+    tempfile = Tempfile.new(using_path ? file.path : "").tap do |t|
       t.write(file.read)
       t.rewind
     end
--- spec/file_to_tempfile_spec.rb
+++ spec/file_to_tempfile_spec.rb
@@ -1,64 +1,62 @@
 # frozen_string_literal: true
 
-RSpec.describe FileToTempfile do
+RSpec.describe FileToTempfile do # rubocop:disable Metrics/BlockLength
   it "has a version number" do
     expect(FileToTempfile::VERSION).not_to be nil
   end
 
-  describe '#self.convert' do
-    context 'when file is not a File object' do
-      it 'raises an ArgumentError' do
+  describe "#self.convert" do # rubocop:disable Metrics/BlockLength
+    let(:file) { File.open ".gitignore" }
+
+    context "when file is not a File object" do
+      it "raises an ArgumentError" do
         expect do
-          FileToTempfile.convert('file')
-        end.to raise_error ArgumentError, 'Expected a File object as the first argument'
+          FileToTempfile.convert("file")
+        end.to raise_error ArgumentError, "Expected a File object as the first argument"
       end
     end
 
-    context 'when file is a File object' do
-      let(:file) { File.open '.gitignore' }
+    context "when file is open" do
+      context "when file has not been read" do
+        it "converts a file to a Tempfile" do
+          tempfile = FileToTempfile.convert(file)
 
-      context 'when file is open' do
-        context 'when file has not been read' do
-          it 'converts a file to a Tempfile' do
-            tempfile = FileToTempfile.convert(file)
-
-            expect(tempfile).to be_a Tempfile
-            expect(file.read).to eq tempfile.read
-            expect(tempfile.path).to start_with "/tmp/"
-          end
-        end
-
-        context 'when file has been read' do
-          before { file.read }
-
-          it 'converts a file to a Tempfile' do
-            tempfile = FileToTempfile.convert(file)
-
-            expect(tempfile).to be_a Tempfile
-            expect(file.read).to eq tempfile.read
-            expect(tempfile.path).to start_with "/tmp/"
-          end
-        end
-
-        context 'when using_path is true' do
-          it 'converts a file to a Tempfile' do
-            tempfile = FileToTempfile.convert(file, true)
-
-            expect(tempfile).to be_a Tempfile
-            expect(file.read).to eq tempfile.read
-            expect(tempfile.path).to start_with "/tmp/#{file.path}"
-          end
+          expect(tempfile).to be_a Tempfile
+          expect(file.read).to eq tempfile.read
+          expect(tempfile.path).to start_with "/tmp/"
         end
       end
 
-      context 'when file has been closed' do
-        before { file.close }
+      context "when file has been read" do
+        before { file.read }
 
-        it 'raises an error' do
-          expect do
-            FileToTempfile.convert(file)
-          end.to raise_error IOError, 'The file must be open before it can be converted'
+        it "converts a file to a Tempfile" do
+          tempfile = FileToTempfile.convert(file)
+
+          expect(tempfile).to be_a Tempfile
+          expect(file.read).to eq tempfile.read
+          expect(tempfile.path).to start_with "/tmp/"
         end
+      end
+
+      context "when using_path is true" do
+        it "converts a file to a Tempfile" do
+          tempfile = FileToTempfile.convert(file, using_path: true)
+
+          expect(tempfile).to be_a Tempfile
+          expect(file.read).to eq tempfile.read
+          expect(tempfile.path).to start_with "/tmp/#{file.path}"
+        end
+      end
+    end
+
+    context "when file has been closed" do
+      before { file.close }
+
+      it "raises an error" do
+        expect do
+          FileToTempfile.convert(file)
+        end.to raise_error IOError, "The file must be open before it can be converted"
       end
     end
   end
