8b3ccc52349fc2dbb424ba2b8e95929b7db128d1
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-05-01 17:21:44 -0300

    feat: Create self.convert method

--- lib/file_to_tempfile.rb
+++ lib/file_to_tempfile.rb
@@ -2,7 +2,22 @@
 
 require_relative "file_to_tempfile/version"
 
+require 'tempfile'
+
 module FileToTempfile
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.convert(file, using_path = false)
+    raise ArgumentError, 'Expected a File object as the first argument' unless file.is_a? File
+    raise ArgumentError, 'Expected a boolean as the second argument' unless [true, false].include?(using_path)
+    raise IOError, 'The file must be open before it can be converted' if file.closed?
+
+    file.rewind if file.eof?
+
+    tempfile = Tempfile.new(using_path ? file.path : '').tap do |t|
+      t.write(file.read)
+      t.rewind
+    end
+
+    file.rewind
+    tempfile
+  end
 end
